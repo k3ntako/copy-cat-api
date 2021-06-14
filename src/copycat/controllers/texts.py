95c51b00c5e0b6afcdd0b0321937038330eb5007
Kentaro Kaneki <kentarokaneki@gmail.com>
2021-06-14 14:35:17 -0400

    Handle edge cases

--- src/copycat/controllers/texts.py
+++ src/copycat/controllers/texts.py
@@ -7,6 +7,9 @@ bp = Blueprint("texts", __name__, url_prefix="/api/texts")
 
 @bp.route('', methods=['POST'])
 def text_upload():
+    assert request.is_json, 'Request body must be a JSON'
+    assert 'text_string' in request.json, '"text_string" parameter is required'
+
     text = Text(request.json['text_string'])
     db.session.add(text)
     db.session.commit()
--- tests/test_text_endpoint.py
+++ tests/test_text_endpoint.py
@@ -30,7 +30,25 @@ def test_post_commits_to_db(app, client):
         text_row = db.session.query(Text).filter_by(text_string='test string 2').first()
         assert text_row.text_string == 'test string 2'
 
-def test_post_rejects_empty_string(app, client):
+def test_post_rejects_request_without_json_body(client):
+    """POST text endpoint should not allow requests without a JSON body"""
+    res = client.post('/api/texts') 
+    
+    assert res.status == '400 BAD REQUEST'
+    assert res.json['error'] == 'Request body must be a JSON'
+
+def test_post_rejects_request_without_text_string_paramter(client):
+    """POST text endpoint should require text_string parameter"""
+    res = client.post(
+        '/api/texts',
+        data=json.dumps(dict()),
+        content_type='application/json'
+    ) 
+    
+    assert res.status == '400 BAD REQUEST'
+    assert res.json['error'] == '"text_string" parameter is required'
+
+def test_post_rejects_empty_string(client):
     """POST text endpoint should not allow empty strings"""
     res = client.post(
         '/api/texts',
@@ -41,7 +59,7 @@ def test_post_rejects_empty_string(app, client):
     assert res.status == '400 BAD REQUEST'
     assert res.json['error'] == "Text must be at least one character long"
 
-def test_post_rejects_only_whitespace(app, client):
+def test_post_rejects_only_whitespace(client):
     """POST text endpoint should not allow string with only whitespace"""
     res = client.post(
         '/api/texts',
@@ -52,7 +70,7 @@ def test_post_rejects_only_whitespace(app, client):
     assert res.status == '400 BAD REQUEST'
     assert res.json['error'] == "Text must be at least one character long"
 
-def test_post_only_allows_strings(app, client):
+def test_post_only_allows_strings(client):
     """POST text endpoint should not allow text_string parameter that is not a string"""
     res = client.post(
         '/api/texts',
@@ -63,7 +81,7 @@ def test_post_only_allows_strings(app, client):
     assert res.status == '400 BAD REQUEST'
     assert res.json['error'] == "Text must be a string"
 
-def test_post_validates_length(app, client):
+def test_post_validates_length(client):
     """POST text endpoint should not allow text longer than the limit"""
     res = client.post(
         '/api/texts',
